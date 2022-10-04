1782d2b84c98113cea4a8df269364dd4f6c5a916
alanssitis <alanchunggt@gmail.com>
2022-10-04 15:47:34 -0400

    more detail

--- resolve_gh_action.py
+++ resolve_gh_action.py
@@ -1,4 +1,15 @@
-import json
 import sys
 
-print(json.loads(sys.argv[1]))
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        sys.exit(f'Error: Usage: {sys.argv[0]} <image id> <image digest>')
+
+    representation = {
+        'image': {
+            'id': sys.argv[1],
+            'digest': sys.argv[2],
+        },
+        'builder': 'testing'
+    }
+
+    print(representation)
