c3bc598dc3443d8c0381b878eb1c17777fd480c1
alanssitis <alanchunggt@gmail.com>
2022-10-04 15:54:23 -0400

    add identifier

--- resolve_gh_action.py
+++ resolve_gh_action.py
@@ -2,14 +2,13 @@ import sys
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
-        sys.exit(f'Error: Usage: {sys.argv[0]} <image id> <image digest>')
+        sys.exit(f'Error: Usage: {sys.argv[0]} <builder info> <image digest>')
 
     representation = {
         'image': {
-            'id': sys.argv[1],
             'digest': sys.argv[2],
         },
-        'builder': 'testing'
+        'builder': sys.argv[1],
     }
 
     print(representation)
