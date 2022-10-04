03a1b711fb01f924953e0aa77bbae635d6e4d318
alanssitis <alanchunggt@gmail.com>
2022-10-04 16:14:34 -0400

    print context

--- resolve_gh_action.py
+++ resolve_gh_action.py
@@ -2,12 +2,15 @@ import sys
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
-        sys.exit(f'Error: Usage: {sys.argv[0]} <builder info> <image digest>')
+        sys.exit(f'Error: Usage: {sys.argv[0]} <last git commit> <image digest>')
 
     representation = {
         'image': {
             'digest': sys.argv[2],
         },
+        'context': {
+            'commit': sys.argv[1],
+            },
         'builder': sys.argv[1],
     }
 
